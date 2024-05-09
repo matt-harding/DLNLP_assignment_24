0968ce9bf8db1ff36a4cf0e103df39a09c74684e
Harding, Matt <matt.harding@refinitiv.com>
2024-05-09 15:02:50 +0100

    Added data preprocessing

---
+++ Utils/TextPreprocessor.py
@@ -0,0 +1,32 @@
+import string
+import pandas as pd
+import nltk
+from nltk.stem import PorterStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+class TextPreprocessor():
+    def clean_text(text):
+        final_string = ''
+
+        # Lowercase text
+        text = text.lower()
+
+        # Remove puncuation
+        translator = str.maketrans('', '', string.punctuation)
+        text = text.translate(translator)
+
+        # Remove stop words
+        text = text.split()
+        useless_words = nltk.corpus.stopwords.words("english")
+        useless_words = useless_words + ['hi', 'im']
+        text_filtered = [word for word in text if not word in useless_words]
+
+        # Lemmatize
+        lem = WordNetLemmatizer()
+        text_stemmed = [lem.lemmatize(y) for y in text_filtered]
+
+        final_string = ' '.join(text_stemmed)
+
+        return final_string
---
+++ main.py
@@ -0,0 +1,34 @@
+import os
+from datasets import load_dataset, get_dataset_split_names
+import pandas as pd
+
+from Utils import TextPreprocessor
+
+# https://huggingface.co/datasets/yahoo_answers_topics
+DATASET = 'yahoo_answers_topics'
+DATA_DIRECTORY = 'Datasets'
+
+'''
+    Step 1. Fetch dataset & Preprocess
+'''
+for split in get_dataset_split_names(DATASET):
+    # load the split of the dataset
+    dataset = load_dataset(DATASET, split=split)
+    # save the split of the dataset to the download directory as json lines file
+    dataset.to_json(os.path.join(DATA_DIRECTORY, f"{split}.jsonl"))
+
+'''
+    Preprocess data
+'''
+for split in get_dataset_split_names(DATASET):
+    df = pd.read_json('Datasets/' + split + '.jsonl', lines=True)
+
+    clean_df = pd.DataFrame(columns=["question_text", "question_topic"])
+    
+    for index, row in df.iterrows():
+        clean_question = TextPreprocessor.clean_text(row['question_title'] + ' ' + row['question_content'])
+        new_row = pd.Series({"question_text": clean_question,
+                "question_topic": row['topic']})
+        clean_df = pd.concat([clean_df, pd.DataFrame([new_row])], ignore_index=True)
+    
+    clean_df.to_csv(os.path.join(DATA_DIRECTORY, f"clean_{split}.csv")) 
